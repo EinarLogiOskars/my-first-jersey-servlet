250f179dd474c6eddcfa9e8d36cea3ac402e48c4
Einar Logi <einarlogi1@gmail.com>
2017-04-27 19:49:31 +0200

    Modified a bit of html code and renamed most html/css/js files

---
+++ src/main/webapp/index.js
@@ -0,0 +1,15 @@
+$(document).ready(function() {
+		$("#createuser").click(function() {
+			$("#div2").load("createUser.html");
+		});
+		
+		$("#showusers").click(function() {
+			$("#div2").load("showUsers.html");
+		});
+		
+		$("#updateuser").click(function() {
+			$("#div2").load("updateUser.html");
+		});
+		
+		
+});
