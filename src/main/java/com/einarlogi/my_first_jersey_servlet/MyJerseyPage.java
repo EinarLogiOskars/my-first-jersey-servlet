308f2215e92e3ec565908b88c5462b6a5bd26cfa
Einar Logi <einarlogi1@gmail.com>
2017-04-27 13:40:21 +0200

    .

--- src/main/java/com/einarlogi/my_first_jersey_servlet/MyJerseyPage.java
+++ src/main/java/com/einarlogi/my_first_jersey_servlet/MyJerseyPage.java
@@ -1,6 +1,7 @@
 package com.einarlogi.my_first_jersey_servlet;
 
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
